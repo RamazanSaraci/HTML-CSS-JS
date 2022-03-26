abdcfb296cd82192c90114c3e0d847eb4f3f69b4
RamazanSaraci <102411204+RamazanSaraci@users.noreply.github.com>
2022-03-26 13:47:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("myButton").onclick=function(){
+    var Name=document.getElementById("inputFirst-Name").value;
+     console.log(Name);
+    var LName=document.getElementById("inputLast-Name").value;
+     console.log(LName);
+    var Email=document.getElementById("inputEmail").value;
+     console.log(Email);
+    var Number=document.getElementById("phone-number").value;
+     console.log(Number);
+    var Company=document.getElementById("company-name").value;
+     console.log(Company);
+    var Help=document.getElementById("help").value;
+    console.log(Help);
+    
+}
